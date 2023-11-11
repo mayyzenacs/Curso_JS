e6bdb7fca7d545936fecf26fd62220de503e4f8e
mayyzenacs <143430528+mayyzenacs@users.noreply.github.com>
2023-11-10 23:15:25 -0300

    ,

---
+++ ex.js
@@ -0,0 +1,8 @@
+const nome = prompt("Digite seu nome");
+document.body.innerHTML += `seu nome é ${nome} <br />`;
+document.body.innerHTML += `seu nome tem ${nome.length} letras <br />`
+document.body.innerHTML += `a segunda letra do seu nome é ${nome[1]} <br />`
+document.body.innerHTML += `primeiro indice da letra a é ${nome.toString().indexOf("a")} <br />`
+document.body.innerHTML += `o ultimo indice da letra a é ${nome.toString().lastIndexOf("a")} <br />`
+document.body.innerHTML += `as ultimas 3 letras do seu nome sao `
+document.body.innerHTML += ``
