0930b09dee15f2f851c9d391c1ac272c61fb1aef
mayyzenacs <143430528+mayyzenacs@users.noreply.github.com>
2023-11-17 22:03:51 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -29,3 +29,17 @@ const { endereco= {rua, numero}} = people
 for (let i = 0; i <= 10; i++) {
     const check = i % 2 === 0 ? "par" : "impar"
 }
+
+function mostraHora () {
+    let data = new Date()
+
+    return data.toLocaleTimeString("pt-BR", {
+        hour12: false})
+}
+
+const timer = setInterval(() => {
+    console.log(mostraHora())
+},1000);
+setTimeout(function() {
+    clearInterval(timer)
+},1000)
