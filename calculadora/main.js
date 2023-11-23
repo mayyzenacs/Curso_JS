5b466f917b0ff5b413b224e3c182926e7a5a29e3
mayyzena012 <143430528+mayyzenacs@users.noreply.github.com>
2023-11-23 10:05:45 -0300

    Update main.js

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,10 +1,26 @@
-const div = document.querySelector(".divisao")
-const paragrafos = div.querySelectorAll("p")
+function Calculadora() {
+    this.display = document.querySelector(".display")
 
-const estilosBody = getComputedStyle(document.body)
-const backgroundColor = estilosBody.backgroundColor
+    this.cliqueBotão = () => {
+        document.addEventListener("click", event => {
+            const elemento = event.target;
+            if(elemento.classList.contains("btn-num")) this.addNumDisplay(elemento)
+            if(elemento.classList.contains("clear")) this.clear(elemento)
+            if(elemento.classList.contains("igual")) this.resultado(elemento)
+            if(elemento.classList.contains("soma")) this.realizarConta(elemento)
+        })
+    },
 
-for (let p of paragrafos) {
-    p.style.backgroundColor = backgroundColor
-    p.style.color = "white"
-}
+    this.addNumDisplay = elemento => this.display.value += elemento.innerText
+
+    this.clear = () => this.display.value = " "
+
+    this.inicia = () => {
+        this.cliqueBotão()
+    }
+
+    }
+
+const calculadora = new Calculadora()
+calculadora.inicia()
+
